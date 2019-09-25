befdbe71a11c64bf3ff93d5f882e2357bc88e87c
Eylon <eylontoda@gmail.com>
2019-09-25 02:17:20 -0300

    Adição dos modelos em JPA do sistema.

---
+++ src/main/java/br/ufpa/facomp/jsf/domain/enumeration/Estado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpa.facomp.jsf.domain.enumeration;
+
+/**
+ *
+ * @author Felipe
+ */
+public enum Estado {
+    AC, AL, AP, AM, BA, CE, DF, ES, GO, 
+    MA, MT, MS, MG, PA, PB, PR, PE, PI, 
+    RJ, RN, RS, RO, RR, SC, SP, SE, TO;    
+}
