6d1ae338c88eedd44c57a6e28ea24a6ebd15bcf9
Eylon <eylontoda@gmail.com>
2019-09-25 17:37:47 -0300

    Adição de Tocantins.

--- src/main/java/br/ufpa/facomp/jsf/domain/enumeration/Estado.java
+++ src/main/java/br/ufpa/facomp/jsf/domain/enumeration/Estado.java
@@ -12,6 +12,7 @@ package br.ufpa.facomp.jsf.domain.enumeration;
 public enum Estado {
     AC, AL, AP, AM, BA, CE, DF, ES, GO, 
     MA, MT, MS, MG, PA, PB, PR, PE, PI, 
-    RJ, RN, RS, RO, RR, SC, SP, SE;    
+    RJ, RN, RS, RO, RR, SC, SP, SE, TO;    
 }
+
 
