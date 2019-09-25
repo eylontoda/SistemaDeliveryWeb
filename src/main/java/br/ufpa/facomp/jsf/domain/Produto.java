591e97f5c49dc7f542647b688d04586f2118ccc6
Eylon <eylontoda@gmail.com>
2019-09-25 12:40:34 -0300

    Adição do Moledo Produto

---
+++ src/main/java/br/ufpa/facomp/jsf/domain/Produto.java
@@ -0,0 +1,59 @@
+package br.ufpa.facomp.jsf.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public String nome;
+    public String descricao;
+    public String foto;
+    public double preco;
+    public boolean disponibilidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+    
+    
+    
+    
+}
