7820ee246febfd06f98756c243ef6a89d4751733
Eylon <eylontoda@gmail.com>
2019-09-25 21:44:27 -0300

    Alteração nas funçoes getter e setter.

--- src/main/java/br/ufpa/facomp/jsf/domain/Cliente.java
+++ src/main/java/br/ufpa/facomp/jsf/domain/Cliente.java
@@ -21,12 +21,7 @@ public class Cliente {
 
     protected Cliente() {}
 
-    public Cliente(String firstName, String lastName) {
-        this.nome = nome;
-        this.CPF = CPF;
-        this.email = email;
-        this.senha = senha;
-    }
+    
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedido;
     @OneToMany(mappedBy = "cliente")
@@ -42,24 +37,78 @@ public class Cliente {
                 id, nome, CPF,email, senha);
     }
 
-// end::sample[]
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getCPF() {
-		return CPF;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public String senha() {
-		return senha;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Pedido> getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(List<Pedido> pedido) {
+        this.pedido = pedido;
+    }
+
+    public List<Contato> getContato() {
+        return contato;
+    }
+
+    public void setContato(List<Contato> contato) {
+        this.contato = contato;
+    }
+
+    public List<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(List<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(List<Produto> produto) {
+        this.produto = produto;
+    }
+
+
 }
 
